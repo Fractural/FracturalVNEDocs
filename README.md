4b1e0d2148ddeb690b39b2e84dfc271401c8cd5f
Atlinx <atlinuxtm@gmail.com>
2021-08-22 01:09:31 -0400

    Add .gitignore, Update README.md, Playing around with docs (Copied from Godot Docs)

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 Documentation for the [Fractural Visual Novel Engine]("https://github.com/Fractural/FracturalVisualNovelEngine").
 
 Built with Sphinx and Read the Docs.
+
+## How to Build
+1. To build it you'll need [Python](https://www.python.org/downloads/), [CMake]("https://cmake.org/install/"), and some other libraries.
+
+2. Install libraries with:
+    `python -m pip install -r requirements.txt`
+
+3. Build:
+    - Linux/Mac:
+        ```bash
+        > make html
+        ```
+    - Windows:
+        ```bat
+        > make.bat html
+        ```
--- conf.py
+++ conf.py
@@ -10,16 +10,18 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('_extensions'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'Fractural Visual Novel Engine'
-copyright = '2021, Fractural Studios, Atlinx'
-author = 'Fractural Studios, Atlinx'
+copyright = '2021, Fractural, Atlinx and contributors'
+author = 'Fractural, Atlinx and contributors'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
@@ -31,7 +33,31 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_parser",
+    "gdscript",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx_panels",
 ]
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    "linkify",
+]
+
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,17 +65,67 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+	"README.md"
+]
 
+# Version info for the project, acts as replacement for |version| and |release|
+# The short X.Y version
+version = os.getenv("READTHEDOCS_VERSION", "latest")
+# The full version, including alpha/beta/rc tags
+release = version
+
+# fmt: off
+# These imports should *not* be moved to the start of the file,
+# they depend on the sys.path.append call registering "_extensions".
+# GDScript syntax highlighting
+from gdscript import GDScriptLexer
+from sphinx.highlighting import lexers
+
+lexers["gdscript"] = GDScriptLexer()
+# fmt: on
+
+smartquotes = False
+
+# Pygments (syntax highlighting) style to use
+pygments_style = "sphinx"
+highlight_language = "gdscript"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if on_rtd:
+    using_rtd_theme = True
+
+
+# Theme options
+html_theme_options = {
+    # if we have a html_logo below, this shows /only/ the logo with no title text
+    "logo_only": True,
+    # Collapse navigation (False makes it tree-like)
+    "collapse_navigation": False,
+}
+
+html_logo = "img/logo_3_transparent.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ['_static']
+
+html_css_files = ["css/custom.css"]
+
+html_js_files = ["js/custom.js"]
+
+
+# Locals
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False     # optional.
